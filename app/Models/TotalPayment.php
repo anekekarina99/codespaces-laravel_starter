1b0cd36b5acae3365fde6e53756dfb0c87e4480e
Ane <42421075+anekekarina99@users.noreply.github.com>
2023-09-06 19:38:52 +0000

    add all model

---
+++ app/Models/TotalPayment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TotalPayment extends Model
+{
+    use HasFactory;
+}
