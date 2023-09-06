bc488e1810f23561879f7ed63b190344da6cdcfc
Ane <42421075+anekekarina99@users.noreply.github.com>
2023-09-06 19:27:03 +0000

    something change

---
+++ app/Models/Users.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    use HasFactory;
+
+    protected $table ='users_fix';
+    // Kolom-kolom yang dapat diisi massal (fillable)
+
+    protected $fillable = [
+        'name',
+        'email',
+    ];
+
+    // Tambahkan relasi atau metode lain yang Anda butuhkan di sini
+}
