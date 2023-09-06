26d0a1972f65d42476780deadd12b577ddb9a992
Ane <42421075+anekekarina99@users.noreply.github.com>
2023-09-06 06:03:35 +0000

    edit syntax and add something in file

--- database/migrations/2023_09_06_053703_create_users_fix_table.php
+++ database/migrations/2023_09_06_053703_create_users_fix_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
         });
-        //Tabel
+        //Tabel Client
         Schema::create('client', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
@@ -28,6 +28,113 @@ return new class extends Migration
             $table->string('alamat');
             $table->string('telepon');
         });
+        //Tabel Pekerjaan
+        Schema::create('work', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name_work');
+        });
+
+        //Tabel Tipe Pekerjaan
+        Schema::create('type_work', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name_type');
+        });
+
+            //Tabel Project
+        Schema::create('project', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->unsignedBigInteger('client_id_p');
+            $table->string('telepon');
+
+            $table->foreign('client_id')->references('id')->on('client');
+        });
+        //Tabel P
+        Schema::create('client', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nama');
+            $table->string('alamat');
+            $table->string('telepon');
+        });
+        //Tabel P
+        Schema::create('client', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nama');
+            $table->string('alamat');
+            $table->string('telepon');
+        });
+        //Tabel P
+        Schema::create('client', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nama');
+            $table->string('alamat');
+            $table->string('telepon');
+        });
+        //Tabel P
+        Schema::create('client', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nama');
+            $table->string('alamat');
+            $table->string('telepon');
+        });
+        //Tabel P
+        Schema::create('client', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nama');
+            $table->string('alamat');
+            $table->string('telepon');
+        });
+        //Tabel P
+        Schema::create('client', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nama');
+            $table->string('alamat');
+            $table->string('telepon');
+        });
+        //Tabel P
+        Schema::create('client', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nama');
+            $table->string('alamat');
+            $table->string('telepon');
+        });
+        //Tabel P
+        Schema::create('client', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nama');
+            $table->string('alamat');
+            $table->string('telepon');
+        });
+        //Tabel P
+        Schema::create('client', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nama');
+            $table->string('alamat');
+            $table->string('telepon');
+        });
+        //Tabel P
+        Schema::create('client', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nama');
+            $table->string('alamat');
+            $table->string('telepon');
+        });
+
     }
 
     /**
